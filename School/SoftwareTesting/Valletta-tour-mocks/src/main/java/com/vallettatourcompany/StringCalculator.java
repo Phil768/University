f60441fa11984fe5b9e1b1659e52db10c306c868
Phil768 <philippaul-2002@outlook.com>
2022-10-05 10:28:54 +0200

    java

--- School/SoftwareTesting/Valletta-tour-mocks/src/main/java/com/vallettatourcompany/StringCalculator.java
+++ School/SoftwareTesting/Valletta-tour-mocks/src/main/java/com/vallettatourcompany/StringCalculator.java
@@ -1,17 +1,51 @@
 package com.vallettatourcompany;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class StringCalculator {
     public int add(String numbers) {
 
         int answer = 0;
+        List<Integer> negativeNumbers = new ArrayList<>();
+
+        if(numbers.contains("\n")) {
+            numbers = numbers.replaceAll("[\\n]", "");
+        }
+
+        String[] numberArray = numbers.split(",");
+
         if(numbers == "") {
             return 0;
         }
-        else {
-            for(int i = 0; i < numbers.length(); i++) {
-                Integer.parseInt(numbers.charAt(0))
-            }
+        else if (numbers.length() == 1) {
+            return Integer.parseInt(numbers);
         }
-        return 10;
+        else {
+            for(int i = 0; i < numberArray.length; i++) {
+                String c = numberArray[i];
+                //handling negative numbers.
+                int intC = Integer.parseInt(numberArray[i]);
+                if(Objects.equals(c, ",") || intC > 1000) { //GAy!!!
+                    continue;
+                }
+                else if(intC < 0 ) {
+                    negativeNumbers.add(intC);
+                }
+                else {
+                    if(negativeNumbers.size() > 0)
+                    {
+                        System.out.println("Negative numbers not allowed: " +Arrays.toString(negativeNumbers.toArray()));
+                        return -1;
+                    }
+                    else {
+                        answer += intC;
+                    }
+                }
+            }
+            return answer;
+        }
     }
 }
--- School/SoftwareTesting/Valletta-tour-mocks/src/test/java/com/vallettatourcompany/TestDrivenDevelopment/StringCalculatorTests.java
+++ School/SoftwareTesting/Valletta-tour-mocks/src/test/java/com/vallettatourcompany/TestDrivenDevelopment/StringCalculatorTests.java
@@ -14,14 +14,62 @@ public class StringCalculatorTests {
 
     @Test
     public void testZeroNumbers() {
-        //Setup done in beforeEach
-        //Exercise
-        calculator.add("");
         //Verify
         Assertions.assertEquals(0, calculator.add(""));
     }
-    @Test
-    public void addTwoStrings() {
 
+    @Test
+    public void testOneNumber() {
+        //Verify
+        Assertions.assertEquals(1, calculator.add("1"));
     }
+    @Test
+    public void testAddTwoStrings() {
+        //verify
+        Assertions.assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    public void testAnyNumberOfStrings() {
+        //verify
+        Assertions.assertEquals(10, calculator.add("2,2,2,2,2"));
+    }
+
+    @Test
+    public void testNumbersWithNewLine() {
+        //Verify
+        Assertions.assertEquals(3, calculator.add("1\n,2"));
+    }
+
+    @Test
+    public void testWithoutNegativeNumbers() {
+        //Verify
+        Assertions.assertEquals(10, calculator.add("2,3,5"));
+    }
+
+    @Test
+    public void testWithOneNegativeNumber() {
+        //Verify
+        Assertions.assertEquals(-1, calculator.add("-1,5,2,2"));
+    }
+
+    @Test
+    public void testWithMultipleNegativeNumbers() {
+        //Verify
+        Assertions.assertEquals(-1, calculator.add("1,-1,-5,-2,-7,2"));
+    }
+
+    @Test
+    public void testNumbersBiggerThanOneThousand() {
+        //Verify
+        Assertions.assertEquals(1, calculator.add("1001,1"));
+    }
+
+    @Test
+    public void testNumbersBiggerThanTenThousand() {
+        //Verify
+        Assertions.assertEquals(1, calculator.add("10000,1"));
+    }
+
+
 }
