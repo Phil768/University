9f21fd77b44fd96f629de32221bf7d1df5deaf7a
Phil768 <81355531+Phil768@users.noreply.github.com>
2022-10-12 10:56:46 +0200

    removed firebase

--- School/MobileDeviceProgramming/reactApp/navigation/screens/HomeScreen.js
+++ School/MobileDeviceProgramming/reactApp/navigation/screens/HomeScreen.js
@@ -4,99 +4,73 @@ import {
   Text,
   TextInput,
   SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
   Alert,
   Button,
+  FlatList,
 } from "react-native";
-import * as firebase from "firebase/app";
-
+import * as SQLite from "expo-sqlite";
 import { styles } from "../../Styles";
+//Returns databse object.
+const db = SQLite.openDatabase("db.testDB");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDlOGl8H_Nx7eoZNTNlx9UEhsUTcOifjhk",
-  authDomain: "reactapp-820ce.firebaseapp.com",
-  projectId: "reactapp-820ce",
-  storageBucket: "reactapp-820ce.appspot.com",
-  messagingSenderId: "712993072723",
-  appId: "1:712993072723:web:7965f28a2f723fb515c8a6",
-  measurementId: "G-TW4VX3ZHC4",
-};
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS Items (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, count INT)"
+      );
+    });
+    //this.fetchData();
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Add Random Name with Counts</Text>
+        <TouchableOpacity onPress={this.newItem}>
+          <Text>Add New Item</Text>
+        </TouchableOpacity>
 
-firebase.initializeApp(firebaseConfig);
+        <ScrollView>
+          {this.state.data &&
+            this.state.data.map((data) => (
+              <View key={data.id}>
+                <Text>
+                  {data.text} - {data.count}
+                </Text>
+                <TouchableOpacity onPress={() => this.increment(data.id)}>
+                  <Text> + </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.delete(data.id)}>
+                  <Text> DEL </Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+        </ScrollView>
+      </View>
+    );
+  }
 
-//database
-//import SQLite from "expo-sqlite";
-//
-//const database = SQLite.openDatabase(
-//  {
-//    name: "MainDB",
-//    version: 1,
-//  },
-//  () => {},
-//  (error) => console.log(error)
-//);
-//
-////States create opbjects which can be edited on screen.
-//export default function HomeScreen({ navigation }) {
-//  const [name, setName] = useState("");
-//  const [age, setage] = useState("");
-//
-//  React.useEffect(() => {
-//    createTable();
-//  }, []);
-//
-//  const createTable = () => {
-//    database.transaction((tx) => {
-//      tx.executeSql(
-//        "CREATE TABLE IF NOT EXISTS " +
-//          "Users " +
-//          "(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Age INTEGER);"
-//      );
-//    });
-//  };
-//
-//  const setData = async () => {
-//    if (name.length == 0 || age.length == 0) {
-//      Alert.alert("Warning!", "Please insert some data.");
-//    } else {
-//      try {
-//        await database.transaction(async (tx) => {
-//          await tx.executeSql("INSERT INTO Users (Name, Age) VALUES (?,?)", [
-//            name,
-//            age,
-//          ]);
-//        });
-//      } catch (error) {
-//        console.log(error);
-//      }
-//    }
-//  };
-//
-//  const getData = async () => {
-//    try {
-//      await database.transaction(async (tx) => {
-//        await tx.executeSql("SELECT * FROM Users"),
-//          [],
-//          (tx, results) => {
-//            var len = results.rows.length;
-//            if (len > 0) {
-//              var userName = results.rows.item(0).Name;
-//              var userAge = results.rows.item(0).Age;
-//              setName(userName);
-//              setage(userAge);
-//            }
-//          };
-//      });
-//    } catch (err) {
-//      console.log(err);
-//    }
-//  };
-export default function HomeScreen({ navigation }) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <TextInput placeholder="Email" style={styles.input} />
-      <TextInput placeholder="Password" style={styles.input} />
-      <Button title="Login" />
-      <Button title="Sign up" />
-    </SafeAreaView>
-  );
+  //fetchData = () => {
+  //  db.transaction((tx) => {
+  //    // sending 4 arguments in executeSql
+  //    tx.executeSql(
+  //      "SELECT * FROM Items",
+  //      null, // passing sql query and parameters:null
+  //      // success callback which sends two things Transaction object and ResultSet Object
+  //      (txObj, { rows: { _array } }) =>
+  //        this.setState({ data: _array })(
+  //          // failure callback which sends two things Transaction object and Error
+  //          txObj,
+  //          error
+  //        ),
+  //      console.log("Error ", error)
+  //    ); // end executeSQL
+  //  }); // end transaction
+  //};
 }
