5baa7b8d524926817fc2840a84ee7b716a45e15e
Philip Paul Grima <philippaul-2002@outlook.com>
2022-10-16 22:19:49 +0200

    a

--- School/FYP/JS Practice/Word-Definition ext/background.js
+++
@@ -1,9 +0,0 @@
-console.log("Background loaded");
-var word = "Test background";
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.data === "ready") {
-    console.log(request.data);
-    chrome.runtime.sendMessage(request.data);
-  }
-});
