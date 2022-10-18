f509230887e6437d6e9b4e0b00a1cb2777a033b8
Phil768 <philippaul-2002@outlook.com>
2022-10-18 19:50:18 +0200

    70

--- School/SoftwareTesting/Assignment 1/src/Main.java
+++ School/SoftwareTesting/Assignment 1/src/Main.java
@@ -1,28 +1,54 @@
+/*import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.*;
+//import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;*/
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.net.http.HttpClient;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        URL url = new URL(" https://api.marketalertum.com/Alert");
-        URLConnection con = url.openConnection();
-        HttpURLConnection http = (HttpURLConnection)con;
-        http.setRequestMethod("POST");
-        http.setDoOutput(true);
+        String urlString = "https://api.marketalertum.com/Alert";
+        URL url = new URL(urlString);
+        //Opening the connection.
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+        String jsonInputString =
+                "{\"alertType\":\"6\"," +
+                 "\"heading\":\"Jumper Windows 11 Laptop\"," +
+                 "\"description\":\"Test\"," +
+                 "\"url\":\"https://www.amazon.co.uk/Windows-Display-Ultrabook-Processor-Bluetooth\", " +
+                 "\"imageUrl\":\"https://m.media-amazon.com/images/I/712Xf2LtbJL._AC_SX679_.jpg\"," +
+                " \"postedBy\":\"01150cc0-eff8-4df5-a549-eb18cf7c6184\"," +
+                " \"priceInCents\":\"24999\"}";
 
-        byte[] out = "{\"alertType\":\"6\",\"heading\":\"Jumper Windows 11 Laptop\",\"description\":\"Test\",\"url\":\"https://www.amazon.co.uk/Windows-Display-Ultrabook-Processor-Bluetooth\", \"imageurl\":\"https://m.media-amazon.com/images/I/712Xf2LtbJL._AC_SX679_.jpg\", \"postedBy\":\"01150cc0-eff8-4df5-a549-eb18cf7c6184\", \"priceInCents\":\"24999\"}" .getBytes(StandardCharsets.UTF_8);
-        int length = out.length;
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
 
-        http.setFixedLengthStreamingMode(length);
-        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        http.connect();
-        try(OutputStream os = http.getOutputStream()) {
-            os.write(out);
+        try(BufferedReader br = new BufferedReader(new InputStreamReader((new URL(urlString)).openConnection().getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
         }
 
 
